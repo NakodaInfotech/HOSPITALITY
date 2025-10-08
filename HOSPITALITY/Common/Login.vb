834a90e29837a97974731177cd9d333d7e3c793f
Moin <sales@nakodainfotech.com>
2025-10-08 13:23:05 +0530

    Login

--- HOSPITALITY/Common/Login.vb
+++ HOSPITALITY/Common/Login.vb
@@ -184,7 +184,7 @@ Public Class Login
                     End If
                 ElseIf ClientName = "KHANNA" Then
                     LEADMANAGEMENT = True
-                    If Now.Date > DateTime.Parse("15.10.2025 00:00") Then
+                    If Now.Date > DateTime.Parse("15.10.2026 00:00") Then
                         Dim DTNEW As DataTable = OBJCMN.Execute_Any_String("UPDATE VERSION SET VERSION_NO='1.0.0000'", "", "")
                         GoTo LINE1
                     End If
